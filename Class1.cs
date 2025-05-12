8a9133fb3ec37a311bc8e5738c7e648169f27ca7
Mohamed <mohamed195575@gmail.com>
2025-05-12 13:22:38 +0300

    Add Hello Git Method

--- Class1.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-class MyClass
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello From Class 1");
-    }
-}
--- Class2.cs
+++ Class2.cs
@@ -1,9 +1,15 @@
 using System;
 
-class MyClass
+class MyClass2
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello From Class 2");
     }
+
+public void PrintHelloGit(){
+
+ Console.WriteLine("Hello Git");
+}
+
 }
